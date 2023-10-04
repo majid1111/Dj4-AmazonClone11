1cb9e7ce140e270a5f7f9a15f75510139b4dce08
Majid Khamis <majid111111mm@gmail.com>
2023-10-04 11:26:19 +0400

    add company model

--- project/settings.py
+++ project/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "taggit",
-    'product',
     "debug_toolbar",
+    'product',
+    'setting',
 ]
 
 MIDDLEWARE = [
