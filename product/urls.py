e01bbc9570e56245c486907afc32eee3845a1444
Majid Khamis <majid111111mm@gmail.com>
2023-10-06 07:07:43 +0400

    add product_list,detail api ,serializers

--- product/urls.py
+++ product/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from .views import ProductList , ProductDetail,BrandList ,BrandDetail,queryst_debug
-
+from .api import product_list_api,product_detail_api
 urlpatterns = [
     path('',ProductList.as_view()),
     path('debug',queryst_debug),
     path('<slug:slug>',ProductDetail.as_view()),
     path('brands/',BrandList.as_view()),
     path('brands/<slug:slug>' , BrandDetail.as_view()),
+
+
+#api 
+    path('api/list',product_list_api),
+    path('api/list/<int:product_id>',product_detail_api),
+
 
 ]
