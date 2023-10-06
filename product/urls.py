2619f48730b46ad9fdca026dfd8e8edcc9cbca4c
Majid Khamis <majid111111mm@gmail.com>
2023-10-06 08:18:03 +0400

    add brand list,detail api

--- product/urls.py
+++ product/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import ProductList , ProductDetail,BrandList ,BrandDetail,queryst_debug
-from .api import product_list_api,product_detail_api,ProductListAPI,ProductDetailAPI
+from .api import ProductListAPI,ProductDetailAPI,BrandListAPI,BrandDetailAPI
 urlpatterns = [
     path('',ProductList.as_view()),
     path('debug',queryst_debug),
@@ -13,5 +13,8 @@ urlpatterns = [
     path('api/list',ProductListAPI.as_view()),
     path('api/list/<int:pk>',ProductDetailAPI.as_view()),
 
+    
+    path('brands/api/list',BrandListAPI.as_view()),
+    path('brands/api/list/<int:pk>',BrandDetailAPI.as_view()),
 
 ]
